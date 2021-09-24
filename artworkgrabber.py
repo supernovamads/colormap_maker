f4ada4161fe8d4490a5f63e7a53e189860a4c689
Madison Smith <madvsmit@iu.edu>
2021-09-24 15:47:57 -0400

    Trying to use the iTunes API to grab cover art, but it keeps hanging at the request. Will try more later.

---
+++ artworkgrabber.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 24 15:23:27 2021
+
+@author: madvsmit
+"""
+import requests
+import json
+from urllib.parse import quote
+
+artist = 'Portugal the Man'
+album= 'Woodstock'
+
+query_term = "%s %s" % (artist, album)
+url = "https://itunes.apple.com/search?term=%s&media=music&entity=album" % quote(query_term)
+
+
+import logging
+
+import http.client
+http.client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+
+
+
+response = requests.get(url).json()
+
+
