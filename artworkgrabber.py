ed71a8fe994916a6885fb44cae059745e4e01d02
Madison Smith <madvsmit@iu.edu>
2021-10-25 16:58:08 -0400

    By golly I think it works

--- artworkgrabber.py
+++ artworkgrabber.py
@@ -7,6 +7,7 @@ Created on Fri Sep 24 15:23:27 2021
 """
 import requests
 import json
+import urllib.request
 from urllib.parse import quote
 import logging
 import http.client
@@ -19,18 +20,29 @@ url = "https://itunes.apple.com/search?term=%s&media=music&entity=album" % quote
 
 
 
-http.client.HTTPConnection.debuglevel = 1
+# http.client.HTTPConnection.debuglevel = 1
 
-# You must initialize logging, otherwise you'll not see debug output.
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
-requests_log = logging.getLogger("requests.packages.urllib3")
-requests_log.setLevel(logging.DEBUG)
-requests_log.propagate = True
-
-
+# # You must initialize logging, otherwise you'll not see debug output.
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
 
 
-response = requests.get(url,timeout=5).json()
 
 
+# response = requests.get(url,timeout=5).json()
+
+
+artwork_url = response['results'][0]['artworkUrl60']
+print('NOW GETTING THE ARTWORK')
+# artwork_response = urllib.request.urlretrieve(artwork_url,'../artwork/'+artist.replace(' ','').lower()+
+#                                               '_'+album.replace(' ','').lower()+'.jpg',timeout=5)
+
+artwork_request = urllib.request.urlopen(artwork_url, timeout=5).read()
+with open('artwork/'+artist.replace(' ','').lower()+'_'+album.replace(' ','').lower()+'.jpg', 'wb') as f:
+    try:
+        f.write(artwork_request.content)
+    except:
+        print('error')
